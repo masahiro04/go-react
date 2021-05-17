f43987369bbf7fae7d07b9796dc663dbe19e3fbb
yanyu <dcjcsk0205@gmail.com>
2021-05-17 14:55:20 +0900

    フッター追加

---
+++ front-app/src/components/organisms/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200">
+       <p className="p-2 text-center text-xs">Copyright © 2019 Webty, inc. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
--- front-app/src/pages/Home.tsx
+++ front-app/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import Footer from '../components/organisms/footer'
+import '../App.css';
+
 export default function Home() {
   return (
     <>
       Home here
+      <Footer />
     </>
   );
 }
