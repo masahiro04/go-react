fd8eef8acbd6add50a78f241ecd0bec5ca907d98
Masahiro Okubo <stmhamachiii@gmail.com>
2021-05-17 14:43:54 +0900

    headerの追加

--- front-app/src/pages/Home.tsx
+++ front-app/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../App.css';
+import Header from '../components/organisms/header';
 
 export default function Home() {
   return (
     <>
+      <Header />
       Home here
     </>
   );
